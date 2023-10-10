8ec03ee0216d865986ec06b56fd6068b0383d2fc
fronald143 <120772118+fronald143@users.noreply.github.com>
2023-10-10 20:55:36 +0800

    Add ReviewNote

--- Review/models.py
+++ Review/models.py
@@ -3,9 +3,19 @@ from Account.models import Customer
 from Product.models import Product
 
 
+class ReviewVote(models.Model):
+    review_vote_id = models.BigAutoField(primary_key=True)
+    choices = [
+        ("NO", "No action"),
+        ("LK", "Liked"),
+        ("DK", "Disliked"),
+    ]
+    vote_type = models.CharField(max_length=2, choices=choices, default="NO")
+
+
 class Review(models.Model):
     review_id = models.BigAutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
+    review_vote = models.ForeignKey(ReviewVote, models.SET_NULL, null=True)
     comment = models.CharField(max_length=100)
